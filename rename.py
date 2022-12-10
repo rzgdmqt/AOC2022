dfc6697238c2f104e0f5451de9ec4843bd94268d
rzgdmqt <gasperzajdela@gmail.com>
2022-12-10 15:05:18 +0100

    removed file

--- rename.py
+++
@@ -1,9 +0,0 @@
-import os
-
-for dr in os.listdir("."):
-  if dr.endswith("day"):
-    a = dr.split("day")[0]
-    if len(a) == 1:
-      os.rename(dr, f"0{a}day")
-    else:
-      os.rename(dr, f"{a}day")
